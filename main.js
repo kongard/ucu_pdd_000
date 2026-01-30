543eb6e6f55113f64f7f7c35bc59e8477874a746
kongard <kongard@yandex.ru>
2026-01-30 13:51:00 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-//import * as THREE from 'https://cdn.jsdelivr.net/npm/three@181/build/three.module.js';
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@181/build/three.module.js';
+//import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
@@ -115,5 +115,6 @@ loader.load(
 
 console.log(THREE.REVISION);
 main();
+
 
 
