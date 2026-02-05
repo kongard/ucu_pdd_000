b31282d68e87bbbc03803c356b3c56a2bec573b2
kongard <kongard@yandex.ru>
2026-02-05 17:18:31 +0300

    q

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ function main() {
 	const near = 0.1;
 	const far = 10000;
 	const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
-	camera.position.set( 0, 0, 0 );
+	camera.position.set( 0, 20, 0 );
 
 	const controls = new OrbitControls( camera, canvas );
 	controls.target.set( 0, 15, 0 );
